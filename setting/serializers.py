79899f0ccdd25a2b1fba0cb771a04bdf9537103a
m.salehi <mohammadsalehi8138@gmail.com>
2024-10-05 20:10:09 +0330

    fix bug load image in settings

--- setting/serializers.py
+++ setting/serializers.py
@@ -3,15 +3,15 @@ from .models import SiteSettings, Banner, SocialMedia
 
 
 class SiteSettingSerializer(serializers.ModelSerializer):
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        if "logo" in representation and representation["logo"]:
-            representation["logo"] = str(instance.logo.url)[1:]
-        if "logo_mobile" in representation and representation["logo_mobile"]:
-            representation["logo_mobile"] = str(instance.logo_mobile.url)[1:]
-        if "fav_icon" in representation and representation["fav_icon"]:
-            representation["fav_icon"] = str(instance.fav_icon.url)[1:]
-        return representation
+    # def to_representation(self, instance):
+    #     representation = super().to_representation(instance)
+    #     if "logo" in representation and representation["logo"]:
+    #         representation["logo"] = str(instance.logo.url)[1:]
+    #     if "logo_mobile" in representation and representation["logo_mobile"]:
+    #         representation["logo_mobile"] = str(instance.logo_mobile.url)[1:]
+    #     if "fav_icon" in representation and representation["fav_icon"]:
+    #         representation["fav_icon"] = str(instance.fav_icon.url)[1:]
+    #     return representation
 
     class Meta:
         model = SiteSettings
@@ -31,11 +31,11 @@ class BannerSerializer(serializers.ModelSerializer):
 
 
 class SocialMediaSerializer(serializers.ModelSerializer):
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        if "image" in representation and representation["image"]:
-            representation["image"] = str(instance.image.url)[1:]
-        return representation
+    # def to_representation(self, instance):
+        # representation = super().to_representation(instance)
+        # if "image" in representation and representation["image"]:
+        #     representation["image"] = str(instance.image.url)[1:]
+        # return representation
 
     class Meta:
         model = SocialMedia
