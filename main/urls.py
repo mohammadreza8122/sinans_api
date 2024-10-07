46ab36b4f58caadb7746c0118e03d725acdefaec
m.salehi <mohammadsalehi8138@gmail.com>
2024-10-07 22:58:26 +0330

    remove ajax_select

--- main/urls.py
+++ main/urls.py
@@ -41,7 +41,6 @@ urlpatterns = (
         path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
         path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
         path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-        path('ajax_select/', include('ajax_select.urls')),
 
         path("admin/", admin.site.urls),
         path('ckeditor/', include('ckeditor_uploader.urls')),
