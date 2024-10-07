977ef9a73e1cb441f6ef1f359ce13abfc2cbdbf5
m.salehi <mohammadsalehi8138@gmail.com>
2024-10-07 22:55:23 +0330

    add created by service in admin

--- service/admin.py
+++ service/admin.py
@@ -10,6 +10,8 @@ from .models import (
 )
 from django.utils.html import format_html
 from django.utils.numberformat import format
+from django.contrib.admin.models import LogEntry, ADDITION
+from django.contrib.contenttypes.models import ContentType
 
 
 class CityInline(admin.TabularInline):
@@ -41,6 +43,8 @@ class CityAdmin(admin.ModelAdmin):
 
 @admin.register(HomeCareService)
 class HomeCareServiceAdmin(admin.ModelAdmin):
+    readonly_fields = ['created_by',]
+
     list_display = ("title", "category", "is_active", "is_deleted")
     search_fields = ("title", "category")
     # list_filter = ("title", "category")
@@ -60,6 +64,18 @@ class HomeCareServiceAdmin(admin.ModelAdmin):
             del actions["delete_selected"]
 
         return actions
+
+
+    def created_by(self, instance):
+        log = LogEntry.objects.filter(object_id=instance.id,
+                                       content_type_id = ContentType.objects.get_for_model(instance).pk,
+                                       action_flag     = ADDITION).first()
+        create_by = format_html('<a class="btn btn-info" href="/admin/user/user/{}/change/">{} {}</a>'.format(
+            log.user.pk, log.user.first_name, log.user.last_name
+        ))
+        return create_by
+    created_by.short_description = "ایحاد شده توسط"
+
 
 
 @admin.register(HomeCareCategory)
